ada337c20244b02180cd4c1fe610cdb00767b09e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-11 20:18:46 +0200

    Update 2.StringLength.js

--- JsAdvanced/Syntax, Functions and Statements/Lab/2.StringLength.js
+++ JsAdvanced/Syntax, Functions and Statements/Lab/2.StringLength.js
@@ -1,1 +1,7 @@
-
+function echoFunction(str1, str2, str3) {
+  let length = str1.length + str2.length + str3.length
+  let avg = length / 3
+  console.log(length)
+  console.log(Math.floor(avg))
+}
+echoFunction('chocolate', 'ice cream', 'cake');
