3d616c3cf654cf830b7566ef1303ab9137129518
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-11 20:43:38 +0200

    Update 4.CircleArea.js

--- JsAdvanced/Syntax, Functions and Statements/Lab/4.CircleArea.js
+++ JsAdvanced/Syntax, Functions and Statements/Lab/4.CircleArea.js
@@ -1,11 +1,11 @@
 function circleArea(arg) {
-  let result = ""
-  let type = typeof arg
+  let result = "";
+  let type = typeof arg;
   if(type == "number"){
     result = (Math.pow(arg, 2) * Math.PI).toFixed(2);
   }else{
-    result = `We can not calculate the circle area, because we receive a ${type}.`
+    result = `We can not calculate the circle area, because we receive a ${type}.`;
   }
-  return result
+  return result;
 }
 console.log(circleArea(5));
