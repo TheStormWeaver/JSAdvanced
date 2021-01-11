c18bfabac22034367eb83350605d8892d1c6e671
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-11 20:26:07 +0200

    Update 3.LargestNumber.js

--- JsAdvanced/Syntax, Functions and Statements/Lab/3.LargestNumber.js
+++ JsAdvanced/Syntax, Functions and Statements/Lab/3.LargestNumber.js
@@ -1,1 +1,12 @@
-
+function echoFunction(num1, num2, num3) {
+  let result = 0
+  if(num1 > num2 && num1 > num3){
+    result = num1
+  }else if(num2 > num1 && num2 > num3){
+    result = num2
+  }else if(num3 > num1 && num3 > num2){
+    result = num3
+  }
+  return result
+}
+console.log(echoFunction(-3, -5, -22.5));
