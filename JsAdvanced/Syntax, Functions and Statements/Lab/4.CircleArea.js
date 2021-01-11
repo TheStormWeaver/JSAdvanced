dc515b8f87da2642f58bbcac7df29bfc0b08945d
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-11 20:39:21 +0200

    Update 4.CircleArea.js

--- JsAdvanced/Syntax, Functions and Statements/Lab/4.CircleArea.js
+++ JsAdvanced/Syntax, Functions and Statements/Lab/4.CircleArea.js
@@ -1,1 +1,11 @@
-
+function circleArea(arg) {
+  let result = ""
+  let type = typeof arg
+  if(type = Number){
+    result = (Math.pow(arg, 2) * Math.PI).toFixed(2);
+  }else{
+    result ='We can not calculate the circle area, because we receive a {type of argument}.'
+  }
+  return result
+}
+console.log(circleArea(5));
