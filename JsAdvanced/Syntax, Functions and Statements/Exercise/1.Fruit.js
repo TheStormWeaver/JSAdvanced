3b17d92d1360e8c8d84ba6fa3ec8ce70673aa858
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-12 18:44:55 +0200

    Update 1.Fruit.js

--- JsAdvanced/Syntax, Functions and Statements/Exercise/1.Fruit.js
+++ JsAdvanced/Syntax, Functions and Statements/Exercise/1.Fruit.js
@@ -1,6 +1,6 @@
 function fruits(fruit, weight, price) {
-  let toKilo = (weight / 1000).toFixed(2)
-  let amountNeeded = (price * toKilo).toFixed(2)
- return `I need $${amountNeeded} to buy ${toKilo} kilograms ${fruit}.`
+  let toKilo = weight / 1000
+  let amountNeeded = price * toKilo
+ return `I need $${amountNeeded.toFixed(2)} to buy ${toKilo.toFixed(2)} kilograms ${fruit}.`
 }
 console.log(fruits('orange', 2500, 1.80));
