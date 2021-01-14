e354eac172e6e57b654940298d3c7fa7219cb62d
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-14 20:03:14 +0200

    Create 3.SumFirstLast.js

---
+++ JsAdvanced/Arrays and Nested Arrays/Lab/3.SumFirstLast.js
@@ -0,0 +1,7 @@
+function sumFirstLast(arr) {
+  let first = Number(arr.shift())
+  let last = Number(arr.pop())
+  let result = first + last
+  return result
+}
+console.log(sumFirstLast(['20', '30', '40']));
