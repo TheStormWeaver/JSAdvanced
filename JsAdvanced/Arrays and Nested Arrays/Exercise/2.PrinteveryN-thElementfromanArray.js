0e29c6db07bc2911375f4efd1d40926cf8497d59
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-15 18:30:30 +0200

    Create 2.PrinteveryN-thElementfromanArray.js

---
+++ JsAdvanced/Arrays and Nested Arrays/Exercise/2.PrinteveryN-thElementfromanArray.js
@@ -0,0 +1,8 @@
+function printNthEle(arr, num) {
+  let result = []
+  for (let i = 0; i < arr.length; i+=num) {
+    result.push(arr[i])
+  }
+  return result
+}
+console.log(printNthEle(["5", "20", "31", "4", "20"], 2));
