7e264627730185dec4bcc4d324d579537035aced
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-15 19:25:16 +0200

    Update 8.SortanArrayby2Criteria.js

--- JsAdvanced/Arrays and Nested Arrays/Exercise/8.SortanArrayby2Criteria.js
+++ JsAdvanced/Arrays and Nested Arrays/Exercise/8.SortanArrayby2Criteria.js
@@ -1,6 +1,6 @@
 function sortByTwoCriteria(arr) {
   let sorted = arr.sort((a, b) => {
-    if(a.length > b.length || b.length > a.length){ // (a.length - b.length != 0)
+    if(a.length > b.length || b.length > a.length){ // (a.length - b.length != 0) - alternative
       return a.length - b.length
     }else{
       return a.localeCompare(b)
