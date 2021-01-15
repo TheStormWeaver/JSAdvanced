9ed4196b2df3a4937c4d8ca2e5779dbef0992a86
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-15 19:36:15 +0200

    Create 5.ExtractIncreasingSubsequencefromArray.js

---
+++ JsAdvanced/Arrays and Nested Arrays/Exercise/5.ExtractIncreasingSubsequencefromArray.js
@@ -0,0 +1,13 @@
+function extractIncreasingArr(arr) {
+  let result = [];
+  let biggest = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (biggest < current) {
+      biggest = current;
+      result.push(biggest);
+    }
+  }
+  return result;
+}
+console.log(extractIncreasingArr([1, 3, 8, 4, 10, 12, 3, 2, 24]));
