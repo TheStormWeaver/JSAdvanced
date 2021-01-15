b9019d3ff6e96cdce9b7b52a3db57d1eda156a4e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-15 19:25:02 +0200

    Create 8.SortanArrayby2Criteria.js

---
+++ JsAdvanced/Arrays and Nested Arrays/Exercise/8.SortanArrayby2Criteria.js
@@ -0,0 +1,11 @@
+function sortByTwoCriteria(arr) {
+  let sorted = arr.sort((a, b) => {
+    if(a.length > b.length || b.length > a.length){ // (a.length - b.length != 0)
+      return a.length - b.length
+    }else{
+      return a.localeCompare(b)
+    }
+  })
+  return sorted.join("\n")
+}
+console.log(sortByTwoCriteria(["alpha", "beta", "gamma"]));
