040d44a404c0dcb51ad9044be252baa6b04f416f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-25 19:02:50 +0200

    Update 1.ListofItems.js

--- JsAdvanced/DOM Manipulation and Events/Lab/ListofItems/1.ListofItems.js
+++ JsAdvanced/DOM Manipulation and Events/Lab/ListofItems/1.ListofItems.js
@@ -1,1 +1,7 @@
-
+function addItem() {
+   let text = document.getElementById('newItemText').value
+   let liElement = document.createElement('li')
+   liElement.textContent = text
+   let ulElement = document.getElementById('items')
+   ulElement.appendChild(liElement)
+}
