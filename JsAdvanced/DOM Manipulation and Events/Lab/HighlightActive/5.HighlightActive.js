bfd2d43255258e510c59c13eb8c2f023f8ff2df2
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-25 21:55:01 +0200

    Update 5.HighlightActive.js

--- JsAdvanced/DOM Manipulation and Events/Lab/HighlightActive/5.HighlightActive.js
+++ JsAdvanced/DOM Manipulation and Events/Lab/HighlightActive/5.HighlightActive.js
@@ -1,1 +1,15 @@
+function focus() {
+  Array.from(document.querySelectorAll("input")).forEach((i) => {
+    i.addEventListener("focus", onFocus);
+    i.addEventListener("blur", onBlur);
+  });
+
+  function onFocus(ev) {
+    ev.target.parentNode.className = "focused";
+  }
+
+  function onBlur(ev) {
+    ev.target.parentNode.className = "";
+  }
+}
 
