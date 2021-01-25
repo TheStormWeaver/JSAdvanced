c5fcfc652a094adcf11c1342cb9f14ff1f185980
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-25 21:22:09 +0200

    Update 4.MouseGradient.js

--- JsAdvanced/DOM Manipulation and Events/Lab/MouseGradient/4.MouseGradient.js
+++ JsAdvanced/DOM Manipulation and Events/Lab/MouseGradient/4.MouseGradient.js
@@ -1,1 +1,9 @@
-
+function attachGradientEvents() {
+    document.getElementById('gradient').addEventListener('mousemove', onMove)
+    let output = document.getElementById('result')
+    function onMove(event){
+        const offsetX = event.offsetX
+        const percent = Math.floor(offsetX / event.target.clientWidth * 100)
+        output.textContent = `${percent}%`   
+    }
+}
