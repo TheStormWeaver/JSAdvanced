0764aa5318c421a831f8165579fb521822b4ea04
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-25 19:34:34 +0200

    Update 3.DeletefromTable.js

--- JsAdvanced/DOM Manipulation and Events/Lab/DeletefromTable/3.DeletefromTable.js
+++ JsAdvanced/DOM Manipulation and Events/Lab/DeletefromTable/3.DeletefromTable.js
@@ -1,1 +1,15 @@
-
+function deleteByEmail() {
+    let email = document.querySelector('input[name="email"]').value
+    let rows = Array.from(document.querySelectorAll('tbody tr'))
+    let deleted = false
+    for (const row of rows) {
+        if(row.children[1].textContent == email){
+            row.parentNode.removeChild(row)
+            deleted = true
+            document.getElementById('result').textContent = 'Deleted.'
+        }
+    }
+    if(deleted == false){
+        document.getElementById('result').textContent = 'Not found.'
+    }
+}
