fdcaf66dda4cb845ea8e14281642db3dd190fe9f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-25 21:37:55 +0200

    Update 2.Add&Delete.js

--- JsAdvanced/DOM Manipulation and Events/Lab/Add&Delete/2.Add&Delete.js
+++ JsAdvanced/DOM Manipulation and Events/Lab/Add&Delete/2.Add&Delete.js
@@ -1,1 +1,22 @@
+function addItem() {
+  const input = document.getElementById('newText');
+  const liElement = createElement('li', input.value);
+  liElement.textContent = document.getElementById("newText").value;
+
+  const deleteBtn = createElement('a', '[Delete]')
+  deleteBtn.href = '#'
+  deleteBtn.addEventListener('click', (ev) => {
+     ev.target.parentNode.remove()
+  })
+  liElement.appendChild(deleteBtn)
+
+  document.getElementById("items").appendChild(liElement);
+  input.value = "";
+
+  function createElement(type, content) {
+    const element = document.createElement(type);
+    element.textContent = content;
+    return element;
+  }
+}
 
