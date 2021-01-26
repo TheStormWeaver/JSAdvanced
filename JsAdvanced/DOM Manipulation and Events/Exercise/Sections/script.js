f9607929b9f5f46a61b7bdaddd4f6cbffa17496e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-26 18:48:40 +0200

    Update script.js

--- JsAdvanced/DOM Manipulation and Events/Exercise/Sections/script.js
+++ JsAdvanced/DOM Manipulation and Events/Exercise/Sections/script.js
@@ -1,1 +1,17 @@
+function create(words) {
+   const content = document.getElementById('content')
 
+   for (let i = 0; i < words.length; i++) {
+      const div = document.createElement('div')
+      const p = document.createElement('p')
+      p.textContent = words[i]
+      p.style.display = 'none'
+      div.appendChild(p) 
+      div.addEventListener("click", function (e){
+         const p = e.target.children[0] || e.target
+         const isVisible = p.style.display == "block"
+         p.style.display = isVisible ? 'none' : 'block'
+      })
+      content.appendChild(div)
+   }
+}
