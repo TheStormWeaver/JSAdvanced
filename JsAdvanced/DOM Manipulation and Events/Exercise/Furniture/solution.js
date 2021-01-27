5e58c7bf35d2ce825003df2e4d57c561d80769e8
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-27 16:20:41 +0200

    Update solution.js

--- JsAdvanced/DOM Manipulation and Events/Exercise/Furniture/solution.js
+++ JsAdvanced/DOM Manipulation and Events/Exercise/Furniture/solution.js
@@ -1,71 +1,76 @@
 function solve() {
-  const textareas = document.querySelectorAll('textarea')
-  const buttons = document.querySelectorAll('button')
-  const body = document.querySelector('tbody')
+  document.querySelector("#exercise > button:nth-child(3)").addEventListener("click", generate);
+  document.querySelector("#exercise > button:nth-child(6)").addEventListener("click", buy);
+  let tableBody = document.querySelector("#exercise > div > div > div > div > table > tbody");
 
-  buttons[0].addEventListener('click', function (e) {
-    const arr = JSON.parse(textareas[0].value)
+  function generate() {
+    let inputJson = document.querySelector("#exercise > textarea").value;
+    let parsedInput = JSON.parse(inputJson);
 
-    for (const el of arr) {
-      const row = document.createElement('tr')
+    for (const obj of parsedInput) {
+      let tr = document.createElement("tr");
+      let td1 = document.createElement("td");
+      let img = document.createElement("img");
+      img.src = obj.img;
 
-      const cellImage = document.createElement('td')
-      const image = document.createElement('img')
-      image.setAttribute('src', el.img) 
-      cellImage.appendChild(image)
+      let td2 = document.createElement("td");
+      td2.textContent = obj.name;
 
-      const cellName = document.createElement('td')
-      const pName = document.createElement('p')
-      pName.textContent = el.name
-      cellName.appendChild(pName)
+      let td3 = document.createElement("td");
+      td3.textContent = obj.price;
 
-      const cellPrice = document.createElement('td')
-      const pPrice = document.createElement('p')
-      pPrice.textContent = el.price
-      cellPrice.appendChild(pPrice)
+      let td4 = document.createElement("td");
+      td4.textContent = obj.decFactor;
 
+      let td5 = document.createElement("td");
+      let input = document.createElement("input");
+      input.type = "checkbox";
+      td5.appendChild(input);
 
-      const cellDecor = document.createElement('td')
-      const pDecor = document.createElement('p')
-      pDecor.textContent = el.decFactor
-      cellDecor.appendChild(pDecor)
+      td1.appendChild(img);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(td5);
 
-
-      const cellCheck = document.createElement('td')
-      const input = document.createElement('input')
-      input.setAttribute('type', 'checkbox')
-      cellCheck.appendChild(input)
-
-      row.appendChild(cellImage)
-      row.appendChild(cellName)
-      row.appendChild(cellPrice)
-      row.appendChild(cellDecor)
-      row.appendChild(cellCheck)
-
-      body.appendChild(row)
+      tableBody.appendChild(tr);
     }
-  })
+  }
 
-  buttons[1].addEventListener('click', function (e){
-    const furniture = Array.from(body.querySelectorAll('input[type=checkbox]:checked')).map(input => input.parentNode.parentNode)
-    const result = {
-      bought: [],
-      totalPrice: 0,
-      decFactorSum: 0
+  function buy() {
+
+    let allInputs = document.getElementsByTagName("input");
+    let output = document.querySelector("#exercise > textarea:nth-child(5)");
+    //console.log(tableBody.children);
+    let bought = [];
+    for (let i = 1; i < allInputs.length; i++) {
+      if (allInputs[i].checked === true) {
+        bought.push(tableBody.children[i]);
+      }
     }
-    for (let row of furniture) {
-      let cells = row.children 
-      console.log(cells)
 
-      const name = cells[1].children[0].textContent
-      result.bought.push(name)
-
-      const price = Number(cells[2].children[0].textContent)
-      result.totalPrice += price
-
-      const factor = Number(cells[3].children[0].textContent)
-      result.decFactorSum += factor
+    let names = [];
+    let totalPrice = 0;
+    let decFactors = [];
+    for (const item of bought) {
+      names.push(item.children[1].textContent);
+      totalPrice += Number(item.children[2].textContent);
+      decFactors.push(Number(item.children[3].textContent));
     }
-    textareas[1].value = `Bought furniture: ${result.bought.join(", ")}\nTotal price:${result.totalPrice.toFixed(2)}\nAverage decoration factor: ${result.decFactorSum/furniture.length}`
-  })
+    
+    output.value += `Bought furniture: ${names.join(", ")}\n`
+    output.value += `Total price: ${totalPrice.toFixed(2)}\n`
+    output.value += `Average decoration factor: ${calculateAvg(decFactors)}`;
+  }
+
+  function calculateAvg(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+
+    var avg = sum / arr.length;
+    return avg;
+  }
 }
