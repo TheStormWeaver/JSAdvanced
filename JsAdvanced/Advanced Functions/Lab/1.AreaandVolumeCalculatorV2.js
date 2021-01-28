1e7e115168f8d5707e26cff516c3b1a5dc012b32
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-28 19:48:23 +0200

    Update 1.AreaandVolumeCalculatorV2.js

--- JsAdvanced/Advanced Functions/Lab/1.AreaandVolumeCalculatorV2.js
+++ JsAdvanced/Advanced Functions/Lab/1.AreaandVolumeCalculatorV2.js
@@ -14,3 +14,18 @@ function solve(area, vol, dataJSON){
   }
   return result
 }
+const example = `[
+  {"x":"1","y":"2","z":"10"},
+  {"x":"7","y":"7","z":"10"},
+  {"x":"5","y":"2","z":"10"}
+  ]`
+  
+console.log(solve(area, vol, example))
+
+function area() {
+  return this.x * this.y;
+};
+
+function vol() {
+  return this.x * this.y * this.z;
+};
