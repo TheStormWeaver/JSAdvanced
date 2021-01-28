94f6d0443eb754b18f4e9b250149622d8f2f2c08
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-28 19:43:57 +0200

    Update 1.AreaandVolumeCalculator.js

--- JsAdvanced/Advanced Functions/Lab/1.AreaandVolumeCalculator.js
+++ JsAdvanced/Advanced Functions/Lab/1.AreaandVolumeCalculator.js
@@ -1,1 +1,16 @@
+function solve(area, vol, dataJSON){
+  const figures = JSON.parse(dataJSON)
 
+  const result = []
+
+  for (let figure of figures) {
+    const objArea = area.call(figure)
+    const objVolume = vol.call(figure)
+    const output = {
+      area: objArea,
+      volume: objVolume 
+    }    
+    result.push(output)
+  }
+  return result
+}
