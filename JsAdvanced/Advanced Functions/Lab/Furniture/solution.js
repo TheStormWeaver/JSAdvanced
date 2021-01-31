801f6789625eb745f02401477d0df00a6819fb74
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-01-31 10:25:33 +0200

    Update solution.js

--- JsAdvanced/Advanced Functions/Lab/Furniture/solution.js
+++ JsAdvanced/Advanced Functions/Lab/Furniture/solution.js
@@ -1,85 +1,68 @@
 function solve() {
-  document.querySelector("button").addEventListener("click", onClickGenerate);
-  document.querySelectorAll("button")[1].addEventListener("click", onclickBuy);
- 
- 
-  function onClickGenerate(event) {
-    let tbody = document.querySelector(".table").querySelector("tbody");
- 
-    let button = event.target;
-    let list = JSON.parse(document.querySelector('textarea').value);
-    for (let i = 0; i < list.length; i++) {
-      let tr = document.createElement("tr");
- 
-      let tdIMG = document.createElement("td");
-      let img = document.createElement("img");
-      img.setAttribute("src", list[i]["img"]);
-      tdIMG.appendChild(img);
-      tr.appendChild(tdIMG);
- 
-      let tdPName = document.createElement("td");
-      let pName = document.createElement("p");
-      pName.innerText = list[i]["name"];
-      tdPName.appendChild(pName);
-      tr.appendChild(tdPName);
- 
-      let tdPPrice = document.createElement("td");
-      let pPrice = document.createElement("p");
-      pPrice.innerText = Number(list[i]["price"]);
-      tdPPrice.appendChild(pPrice);
-      tr.appendChild(tdPPrice);
- 
-      let tdDecorationFactor = document.createElement("td");
-      let pDecorationFactor = document.createElement("p");
-      pDecorationFactor.innerText = Number(list[i]["decFactor"]);
-      tdDecorationFactor.appendChild(pDecorationFactor);
-      tr.appendChild(tdDecorationFactor);
- 
-      let tdCheckBox = document.createElement("td");
-      let checkBox = document.createElement("input");
-      checkBox.setAttribute("type", "checkbox");
-      // make checkbox disabled or enabled
-      tdCheckBox.appendChild(checkBox);
-      tr.appendChild(tdCheckBox);
- 
-      tbody.appendChild(tr);
-    }
- 
-  }
- 
-  function onclickBuy(event) {
- 
-    let output = {
-      furniture: [],
-      totalPrice: 0,
-      decFactors: [],
-      getAvgDecFactor() {
-        if(this.decFactors.length == 0){
-          return 0;
+    const [input, output] = [...document.querySelectorAll('textarea')];
+    const table = document.querySelector('table.table tbody');
+    const [generateBtn, buyBtn] = [...document.querySelectorAll('button')];
+
+    const furniture = [];
+
+    generateBtn.addEventListener('click', () => {
+        const furnitureArray = JSON.parse(input.value.trim());
+        table.innerHTML = '';
+        furnitureArray.forEach(f => {
+            const item = createRow(f);
+            furniture.push(item);
+            table.appendChild(item.element);
+        });
+    });
+
+    buyBtn.addEventListener('click', () => {
+        furniture.forEach(f => console.log(f.getValues().name, f.isChecked()));
+    });
+
+    const td = e.bind(null, 'td');
+
+    function createRow(data) {
+        const img = e('img');
+        img.src = data.img;
+
+        const check = e('input');
+        check.type = 'checkbox';
+
+        const element = e('tr',
+            td(img),
+            td(e('p', data.name)),
+            td(e('p', data.price)),
+            td(e('p', data.decFactor)),
+            td(check)
+        );
+
+        return {
+            element,
+            isChecked,
+            getValues
+        };
+
+        function isChecked() {
+            return check.checked;
         }
-        let avgDecFactor = 0;
-        this.decFactors.forEach(a => avgDecFactor += a)
-        return avgDecFactor / this.decFactors.length;
-      },
-      toString() {
-        return `Bought furniture: ${this.furniture.join(", ")}\nTotal price: ${this.totalPrice.toFixed(2)}\nAverage decoration factor: ${this.getAvgDecFactor()}`;
-      }
+
+        function getValues() {
+            return data;
+        }
     }
- 
-    let checkboxes = Array.from(document.querySelector(".table").querySelectorAll("input"));
- 
-    for (let i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-        let rows = checkboxes[i].parentElement.parentElement.querySelectorAll("td");
- 
-          output.furniture.push(rows[1].textContent);
-          output.totalPrice += Number(rows[2].textContent);
-          output.decFactors.push(Number(rows[3].textContent));
-      
-      }
+
+    function e(type, ...content) {
+        const result = document.createElement(type);
+
+        content.forEach(e => {
+            if (typeof e == 'string') {
+                const node = document.createTextNode(e);
+                result.appendChild(node);
+            } else {
+                result.appendChild(e);
+            }
+        });
+
+        return result;
     }
- 
-    document.querySelectorAll("textarea")[1].value = output.toString();
-  }
 }
- 
