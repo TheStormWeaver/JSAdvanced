86c77e10375738f6a9d48fc9946b49fbd80cbc11
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-01 22:29:28 +0200

    Create 4.SumofNumbers.js

---
+++ JsAdvanced/Unit Testing and Error Handling/Lab/4.SumofNumbers.js
@@ -0,0 +1,17 @@
+const { epxect, expect } = require("chai");
+const sum = require("./Project");
+
+describe("Sum Numbers", () => {
+  it("sums single num", () => {
+    expect(sum([1])).to.equal(1)
+  })
+
+  // Test Overloading
+  it("sums multiple nums", () => {
+    expect(sum([1, 1])).to.equal(2)
+  })
+  
+  it("sums diffrent nums", () => {
+    expect(sum([2, 3, 4])).to.equal(9)
+  })
+});
