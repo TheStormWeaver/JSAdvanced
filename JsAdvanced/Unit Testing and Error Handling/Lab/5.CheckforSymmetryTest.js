b677475125b0e466acf7f6e20ac52549ab4dcd18
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-02 12:50:39 +0200

    Create 5.CheckforSymmetryTest.js

---
+++ JsAdvanced/Unit Testing and Error Handling/Lab/5.CheckforSymmetryTest.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const isSymmetric = require("./Project");
+
+describe("isSymmetric", () => {
+  it('return true for valid symmetric input', () => {
+    expect(isSymmetric([1, 1])).to.equal(true)
+  })
+
+  it('returns false for valid non-symmetric input', () => {
+    expect(isSymmetric([1, 2])).to.equal(false)
+  })
+
+  it('returns false for invalid input', () => {
+    expect(isSymmetric('a')).to.equal(false)
+  })
+
+  //test overloading
+  it('give three parameters', () =>{
+    expect(isSymmetric([1, 1, 1])).to.equal(true)
+  })
+
+  it('returns false for invalid input', () => {
+    expect(isSymmetric(['a', 'a'])).to.equal(true)
+  })
+
+  it('returns false for invalid input', () => {
+    expect(isSymmetric(['a', 'b'])).to.equal(false)
+  })
+
+  it('returns false for different types of elements', () => {
+    expect(isSymmetric(['1', 1])).to.equal(false)
+  })
+})
