bf092512e6db7d1c8cf6f0b608ee7a57c4a18183
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-04 20:38:23 +0200

    Update 3.Circle.js

--- JsAdvanced/Classes and Attributes/Lab/3.Circle.js
+++ JsAdvanced/Classes and Attributes/Lab/3.Circle.js
@@ -1,26 +1,26 @@
 function solve() {
-    class Circle {
-        constructor(r){
-            this.getR = () => {
-                return r
-            }
-            this.setR = (value) => {
-                r = value
-            }
+  class Circle {
+    constructor(r) {
+      this.getR = () => {
+        return r;
+      };
+      this.setR = (value) => {
+        r = value;
+      };
 
-            this.getDiameter = () => {
-                return r * 2
-            }
-            this.setDiameter = (value) => {
-                this.setR(value/2)
-            }
-        }
-        get area(){
-            return this.getR() ** 2 * Math.PI
-        }
+      this.getDiameter = () => {
+        return r * 2;
+      };
+      this.setDiameter = (value) => {
+        this.setR(value / 2);
+      };
     }
-    const myCircle = new Circle(3)
-    console.log(myCircle.getR)
-    console.log(myCircle.getDiameter)
+    get area() {
+      return this.getR() ** 2 * Math.PI;
+    }
+  }
+  const myCircle = new Circle(3);
+  console.log(myCircle.getR);
+  console.log(myCircle.getDiameter);
 }
 solve();
