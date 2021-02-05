7faaa190a4cd1edce498678f85280d688b57d3c6
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-05 18:50:48 +0200

    Create 5.LengthLimit.js

---
+++ JsAdvanced/Classes and Attributes/Exercise/5.LengthLimit.js
@@ -0,0 +1,28 @@
+class Stringer {
+  constructor(string, length) {
+    this.innerString = string;
+    this.innerLength = length;
+  }
+  increase(length) {
+    this.innerLength += length;
+  }
+
+  decrease(length) {
+    this.innerLength -= length;
+    if (this.innerLength < 0) {
+      this.innerLength = 0;
+    }
+  }
+
+  toString() {
+    let result = "";
+    if (this.innerLength === 0) {
+      result = "...";
+    } else if (this.innerLength < this.innerString.length) {
+      result = this.innerString.slice(0, this.innerLength) + "...";
+    } else {
+      result = this.innerString;
+    }
+    return result;
+  }
+}
