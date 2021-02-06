6ba7789ec18aae7b56064412b0c57b23cda0c60a
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-06 14:17:27 +0200

    Update 5.Table–SearchEngine.js

--- JsAdvanced/DOM Introduction/Exercise/Table–SearchEngine/5.Table–SearchEngine.js
+++ JsAdvanced/DOM Introduction/Exercise/Table–SearchEngine/5.Table–SearchEngine.js
@@ -1,12 +1,12 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
    const  rows = document.querySelectorAll('tbody tr')
    
    function onClick() {
       const input = document.querySelector('#searchField').value.toLowerCase()
       for (const row of rows) {
-         if(row.textContent.toLowerCase().includes(input)){
+         console.log(input)
+      if(row.textContent.toLowerCase().includes(input) && input != ""){
             row.setAttribute('class', 'select')
          }else{
             row.removeAttribute('class')
