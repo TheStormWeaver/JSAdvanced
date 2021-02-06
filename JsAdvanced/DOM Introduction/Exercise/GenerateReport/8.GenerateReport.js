957438ea9d57b9c7b9853d082c6eca53e55ed22e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-06 15:00:22 +0200

    Update 8.GenerateReport.js

--- JsAdvanced/DOM Introduction/Exercise/GenerateReport/8.GenerateReport.js
+++ JsAdvanced/DOM Introduction/Exercise/GenerateReport/8.GenerateReport.js
@@ -1,24 +1,22 @@
 function generateReport(colNames) {
     let checkedElements = {}
-    Array.from(document.querySelectorAll("input")).map((x,index) =>{
+    let result = []
+    Array.from(document.querySelectorAll("input")).map((x, index) => {
         if(x.checked){
             let name = x.parentElement.textContent.toLowerCase().trim()
             checkedElements[name] = index
         }
     })
-    let result = []
-    let collums = [...document.querySelectorAll("tr")].slice(1).map(row =>{
+    let collums = [...document.querySelectorAll('tr')].slice(1).map(row => {
         let current = {}
- 
-        let rows = [...row.querySelectorAll("td")].map((x,i) =>{
-            Object.keys(checkedElements).map(element =>{
- 
+        let rows = [...row.querySelectorAll('td')].map((x,i) => {
+            Object.keys(checkedElements).map(element => {
                 if(i == checkedElements[element]){
                     current[element] = x.textContent
                 }
             })
         })
         result.push(current)
-    }) 
-        document.getElementById("output").value = JSON.stringify(result)
+    })
+    document.getElementById("output").value = JSON.stringify(result, null, 2)
 }
