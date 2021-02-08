a3a70bf85c2441efc07ce11112c9b4831e68cc52
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-08 19:12:25 +0200

    Create 1.PersonV2.js

---
+++ JsAdvanced/Prototypes and Inheritance/Lab/1.PersonV2.js
@@ -0,0 +1,31 @@
+function Person(firstName, lastName){
+    this.firstName = firstName
+    this.lastName = lastName
+
+    Object.defineProperties(this, "fullName", {
+        enumerable: true,
+        get: function(){
+            return  `${this.firstName} ${this.lastName}`
+        },
+        set: function(value){
+            const tokens = value.split(" ")
+            this.firstName = tokens[0]
+            this.lastName = tokens[1]
+        }
+    })
+}
+
+const myPerson = new Person("May", "Sure")
+console.log(myPerson.fullName)
+
+myPerson.lastName = "Jackson"
+
+console.log(myPerson.fullName)
+
+myPerson.fullName = "May Sure"
+
+console.log(myPerson.firstName)
+console.log(myPerson.lastName)
+
+
+
