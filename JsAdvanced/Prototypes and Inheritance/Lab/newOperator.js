0bbe895734882444fa8b4db4606c94cdd704950d
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-08 20:37:11 +0200

    Update newOperator.js

--- JsAdvanced/Prototypes and Inheritance/Lab/newOperator.js
+++ JsAdvanced/Prototypes and Inheritance/Lab/newOperator.js
@@ -1,6 +1,25 @@
-//Person is a examplary class
+/*
+function Person(firstName, lastName){
+    this.firstName = firstName
+    this.lastName = lastName
+
+    Object.defineProperty(this, "fullName", {
+        enumerable: true,
+        get: function(){
+            return  `${this.firstName} ${this.lastName}`
+        },
+        set: function(value){
+            const tokens = value.split(" ")
+            this.firstName = tokens[0]
+            this.lastName = tokens[1]
+        }
+    })
+}
+const myPerson = new Person("May", "Sure")
+console.log(myPerson)
+*/
+ //function used to replace the "new" operator in classes
 function newOperator(constructor, ...params){
-    //function used to replace the "new" operator in classes
     const result = {}
     Object.setPrototypeOf(result, Person.prototype)
     constructor.apply(result, params)
@@ -9,8 +28,31 @@ function newOperator(constructor, ...params){
 
 //V2
 function newOperator(constructor, ...params){
-    //function used to replace the "new" operator in classes
+   
     const result = Object.create(Person.prototype)
     constructor.apply(result, params)
     return result
 }
+
+/* Without Prototype (V3)
+function Person(firstName, lastName){
+    this.firstName = firstName
+    this.lastName = lastName
+}
+
+const myProto = {
+    write(message){
+        console.log(`${this.firstName} wrote: ${message}`)
+    }
+}
+const myPerson = new Person("May", "Sure")
+console.log(myPerson)
+*/
+function newOperator(constructor, ...params){
+    //function used to replace the "new" operator in classes
+    const result = Object.create(myProto)
+    constructor.apply(result, params)
+    return result
+}
+
+
