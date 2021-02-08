d6b7b9cdf78e4a6be4c25334d9bbf0859f2aee2e
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-08 20:31:37 +0200

    Update newOperator.js

--- JsAdvanced/Prototypes and Inheritance/Lab/newOperator.js
+++ JsAdvanced/Prototypes and Inheritance/Lab/newOperator.js
@@ -6,3 +6,11 @@ function newOperator(constructor, ...params){
     constructor.apply(result, params)
     return result
 }
+
+//V2
+function newOperator(constructor, ...params){
+    //function used to replace the "new" operator in classes
+    const result = Object.create(Person.prototype)
+    constructor.apply(result, params)
+    return result
+}
