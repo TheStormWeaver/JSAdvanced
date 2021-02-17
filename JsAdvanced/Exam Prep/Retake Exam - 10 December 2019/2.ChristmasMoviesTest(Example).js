e720be60ab8caddfc0119f1be25276c7b527a3a0
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-17 11:57:10 +0200

    Update 2.ChristmasMoviesTest(Example).js

--- JsAdvanced/Exam Prep/Retake Exam - 10 December 2019/2.ChristmasMoviesTest(Example).js
+++ JsAdvanced/Exam Prep/Retake Exam - 10 December 2019/2.ChristmasMoviesTest(Example).js
@@ -2,39 +2,77 @@ const { expect, assert } = require("chai");
 const ChristmasMovies = require("./Project");
 
 describe("Christmas Movies", function () {
-  let christmas = new ChristmasMovies();
-
-  it("Testing the constructor", function () {
-    expect(christmas).to.have.property("movieCollection")
-    expect(christmas).to.have.property("watched")
-    expect(christmas).to.have.property("actors")
+  let movie;
+  beforeEach(function () {
+      movie = new ChristmasMovies;
   });
 
-  it("Testing buyMovie", function () {
-    expect(christmas.buyMovie('Last Christmas', ['Madison Ingoldsby', 'Emma Thompson', 'Boris Isakovic', 'Madison Ingoldsby'])).to.equal("You just got Last Christmas to your collection in which Madison Ingoldsby, Emma Thompson, Boris Isakovic are taking part!")
-    expect(christmas.buyMovie('Home Alone', ['Macaulay Culkin', 'Joe Pesci', 'Daniel Stern'])).to.equal("You just got Home Alone to your collection in which Macaulay Culkin, Joe Pesci, Daniel Stern are taking part!")
-    expect(christmas.buyMovie('Home Alone 2', ['Macaulay Culkin'])).to.equal("You just got Home Alone 2 to your collection in which Macaulay Culkin are taking part!")
-    expect(christmas.buyMovie('The Grinch', ['Benedict Cumberbatch', 'Rashida Jones'])).to.equal("You just got The Grinch to your collection in which Benedict Cumberbatch, Rashida Jones are taking part!")
+  describe("Testing the constructor", function () {
+    it("Testing the 3 base values", function () {
+    expect(movie).to.have.property("movieCollection")
+    expect(movie).to.have.property("watched")
+    expect(movie).to.have.property("actors")
+    })
   });
 
-  it("Testing watchMovie", function(){
-    expect(christmas.watchMovie("The Grinch")).to.equal(undefined)
-    expect(christmas.watchMovie("Home Alone")).to.equal(undefined)
-    expect(christmas.watchMovie("Home Alone")).to.equal(undefined)
-    expect(christmas.watchMovie("Home Alone")).to.equal(undefined)
-    expect(christmas.watchMovie("Last Christmas")).to.equal(undefined)
-    expect(christmas.watchMovie("Last Christmas")).to.equal(undefined)
+  describe("Testing buyMovie", function () {
+    it("Corect inputs", function (){
+      expect(movie.buyMovie("Hello", ["Joe", "Danny"])).to.equal("You just got Hello to your collection in which Joe, Danny are taking part!")
+      expect(movie.buyMovie("This World", ["Max", "Scott"])).to.equal("You just got This World to your collection in which Max, Scott are taking part!")
+      expect(movie.buyMovie("This Place")).to.equal("You just got This Place to your collection in which  are taking part!")
+      expect(movie.buyMovie(0)).to.equal("You just got 0 to your collection in which  are taking part!")
+    })
+    it("Error for adding the same movie", function(){
+      movie.buyMovie("Hello", ["Joe", "Danny"])
+      expect(() => movie.buyMovie("Hello", ["Joe", "Danny"])).to.throw(`You already own Hello in your collection!`)
+    })
+  });
+
+  describe("Testing discardMovie", function(){
+    it("Error for not owning movie", function(){
+    expect(() => movie.discardMovie("Stuff")).to.throw(`Stuff is not at your collection!`)
+  })
+  it("Not Watched Error", function(){
+    movie.buyMovie("Hello", ["Joe", "Danny"])
+    expect(() => movie.discardMovie("Hello")).to.throw(`Hello is not watched!`)
+  })
+  it("Not Watched Error", function(){
+    movie.buyMovie("Hello", ["Joe", "Danny"])
+    movie.watchMovie("Hello")
+    expect(movie.discardMovie("Hello")).to.equal(`You just threw away Hello!`)
+  })
+ })
+
+  describe("Testing watchMovie", function(){
+    it("Error for not having the movie", function(){
+    expect(() => movie.watchMovie("Stuff")).to.throw(`No such movie in your collection!`)
+    })
+    it("Adding +1 to watchMovie", function(){
+      movie.buyMovie("Hello", ["Joe", "Danny"])
+      expect(movie.watchMovie("Hello")).to.equal(undefined)
+      expect(movie.watched["Hello"]).to.equal(1)
+    })
   })
 
-  it("Testing discardMovie", function(){
-    expect(christmas.discardMovie('The Grinch')).to.equal("You just threw away The Grinch!")
+  describe("Testing favoriteMovie", function(){
+    it("Error for not having watched any movies", function(){
+      expect(() => movie.favouriteMovie()).to.throw('You have not watched a movie yet this year!')
+    })
+    it("Favorite movie being Hello", function(){
+      movie.buyMovie("Hello", ["Joe", "Danny"])
+      movie.watchMovie("Hello")
+      expect(movie.favouriteMovie()).to.equal('Your favourite movie is Hello and you have watched it 1 times!')
+    })
   })
 
-  it("Testing favoriteMovie", function(){
-    expect(christmas.favouriteMovie()).to.equal("Your favourite movie is Home Alone and you have watched it 3 times!")
-  })
-
-  it("Testing mostStarredActors", function (){
-    expect(christmas.mostStarredActor()).to.equal("The most starred actor is Macaulay Culkin and starred in 2 movies!")
+  describe("Testing mostStarredActors", function (){
+    it("Error for not having watched any movies", function(){
+      expect(() => movie.mostStarredActor()).to.throw('You have not watched a movie yet this year!')
+    })
+    it("Most starred actor being Joe", function(){
+      movie.buyMovie("Hello", ["Joe", "Danny"])
+      movie.buyMovie("Welcome To MY World!", ["Joe", "Max"])
+      expect(movie.mostStarredActor()).to.equal(`The most starred actor is Joe and starred in 2 movies!`)
+    })
   })
 });
