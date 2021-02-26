d701de8e84b3e09f7ae5a8be19c5760be5734699
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-02-26 14:00:43 +0200

    Create app(await).js

---
+++ JsApplications/Asynchronous Programming/Lab/Github Repos/app(await).js
@@ -0,0 +1,15 @@
+async function loadRepos() {
+  const username = document.getElementById("username").value;
+
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  const response = await fetch(url);
+  const data = await response.json()
+  const ulElement = document.getElementById("repos");
+  ulElement.innerHTML = "";
+  data.forEach((r) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = r.full_name;
+    ulElement.appendChild(liElement);
+  });
+}
